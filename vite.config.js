4071a822085c99f454cd1f810167f9c23fa758a3
Jermaine Rateng <ratengjermaine@gmail.com>
2026-04-24 17:18:29 +0300

    Fix blank page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change back to '/' for custom domains (e.g. jermainerateng.com)
+  base: '/portfolio/', // Correct base path for ratengjermaine.github.io/portfolio/
 })
